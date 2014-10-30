3b93fe773d3884d1bb32ae33f61a29006fea496d
nzkopraq <murthar@abv.bg>
2014-10-30 19:01:57 +0200

    programs durhur

---
+++ Source (2).cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int numb;
+	cout<<"Gimme a number"<<endl;
+	cin>> numb;
+	cout<<numb<<" is a dumb number to choose"<<endl;
+	system("pause");
+	return 0;
+}
---
+++ Source (3).cpp
@@ -0,0 +1,8 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"Hello FMI"<<endl;
+	system ("pause");
+	return 0;
+}
