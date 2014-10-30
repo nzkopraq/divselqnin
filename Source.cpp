ba9a24dbed4c01848eb7f2abce9b73afa7bf7d63
nzkopraq <murthar@abv.bg>
2014-10-30 18:46:12 +0200

    my first commit

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ bool loveprograming=false, loveludogorets=false, programshouldhavenocomments=true;
+cout<<programshouldhavenocomments<<endl;
+cout<<loveludogorets<<endl;
+cout<<loveprograming<<endl;
+ system("pause");
+ return 0;
+}
